f5b16ab61de713f834bc1a1df274784fd481dcb4
Roaa Hasan <85456781+Roaa-Hasan@users.noreply.github.com>
2021-07-30 00:15:30 +0300

    Add files via upload

---
+++ DB1.sql
@@ -0,0 +1,43 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `control_pannel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `conrol_pannel_table`
+--
+
+CREATE TABLE `conrol_pannel_table` (
+  `Forwards` varchar(8) NOT NULL,
+  `Left` varchar(4) NOT NULL,
+  `STOP` varchar(4) NOT NULL,
+  `Right` varchar(4) NOT NULL,
+  `Backwards` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `conrol_pannel_table`
+--
+
+INSERT INTO `conrol_pannel_table` (`Forwards`, `Left`, `STOP`, `Right`, `Backwards`) VALUES
+('', '', 'S', '', ''),
+('', 'L', '', '', ''),
+('F', '', '', '', ''),
+('', '', '', 'R', ''),
+('', '', '', '', 'B');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
